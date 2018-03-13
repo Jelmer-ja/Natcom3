490aafa5cfeb3807cbc32c361195ca1f95647092
Tristan Payer <t.payer@gmx.de>
2018-03-13 16:05:59 +0100

    adding thte beginning of the aco sudoku

---
+++ sudoku/sudoku.py
@@ -0,0 +1,75 @@
+#import pandas as pd
+import numpy as np
+
+# a 9x9 (i,j) grid
+sudoku_field = []
+
+# load the grid from the .txt field
+with open('s10a.txt') as f:
+    for line in f:
+        int_list = [int(i) for i in line.split()]
+        sudoku_field.append(int_list)
+
+# print the field
+for i, row in enumerate(sudoku_field):
+    print(i, " ", row)
+
+#max amount of pheromone that can be placed on a cell
+max_pheromone = 1000
+
+# used to store the amount of pheromone per digit/cell combination
+pheromone_tabel = np.ones((9,9,9)) * max_pheromone
+
+def update_m()
+
+# check row for possible solutions
+def check_row_wise(field, row_idx):
+    solutions = np.arange(1,10)
+    row = field[row_idx]
+    for cell in row:
+        if cell != 0:
+            solutions = np.delete(solutions, cell-1)
+    return solutions
+
+
+# col for possible solutions
+def check_col_wise(field, col_idx):
+    solutions = np.arange(1, 10)
+    col = np.ones((9,), dtype=int)
+    for i, row in field:
+        col[i] = row[col_idx]
+    for cell in col:
+        if cell != 0:
+            solutions = np.delete(solutions, cell - 1)
+    return solutions
+
+
+# check sub-square for possible solutions:
+# TODO: improve!
+def check_sub_square(field, row_idx, col_idx):
+    solutions = np.arange(1, 10)
+    sub_square = np.ones((9,), dtype=int)
+    new_row_idx, new_col_idx = 0
+
+    if row_idx in range(0,3):
+        new_row_idx = 0
+    elif row_idx in range(3,6):
+        new_row_idx = 3
+    elif new_row_idx in range(6,9):
+        new_row_idx = 6
+
+    if col_idx in range(0, 3):
+        new_col_idx = 0
+    elif col_idx in range(3, 6):
+        new_col_idx = 3
+    elif col_idx in range(6, 9):
+        new_col_idx = 6
+
+    for i in range(row_idx, row_idx+3):
+        for j in range(col_idx, col_idx+3):
+            sub_square[i+j] = field[i][j]
+
+    for cell in sub_square:
+        if cell != 0:
+            solutions = np.delete(solutions, cell - 1)
+    return solutions
